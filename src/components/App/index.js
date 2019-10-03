bcdcc5c6046359eb27f2a4bc1e80e4412b2237ab
Alexander Panteleev <746590@mail.ru>
2019-10-03 15:50:43 +0300

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Wrapper, Title} from './styled';
+
+const App = () => (
+    <Wrapper>
+        <Title>React Boilerplate</Title>
+    </Wrapper>
+);
+
+export default App;
